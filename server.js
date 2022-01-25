adb74fe542df511f4005f764c9d714be2afb6eed
Alfrugo <adiez@mtm-inc.net>
2022-01-25 15:19:13 -0600

    2nd

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const htmlroutes = require ('./routes/htmlroutes')
 const apiroutes = require ('./routes/apiroutes');
 const { json } = require('express');
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 // Initialize our app variable by setting it to the value of express()
 const app = express();
